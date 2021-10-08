4ab3b184c952632a3550838762bf323e1eefab38
rhwmunif <92174741+rhwmunif@users.noreply.github.com>
2021-10-08 23:52:13 +0700

    Add files via upload

---
+++ PWDK1/upload/Untitled-1.php
@@ -0,0 +1,9 @@
+<?
+$myDir = "c:/xamp/upload";
+$dir = opendir($myDir);
+echo "Isi folder (klik link untuk download : <br>";
+while($tmp = readdir($dir)){
+echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
+}
+closedir($dir);
+?>
